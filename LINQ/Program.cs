09083ec162992076e239db720afeaa510fc47af3
Darron Manning <darronmanning@yahoo.com>
2025-08-09 13:29:39 -0500

    LinqExercise

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -1,10 +1,28 @@
-﻿namespace LINQ
+﻿using System.Linq;
+using System.Collections.Generic;
+using System; 
+
+
+namespace LINQ
+
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<string> gameNames = new List<string>() 
+                { "Grand Theft Auto v", "Call of Duty", "Super Mario Bros", "The Last of Us" };
+            gameNames.Add("Spider-Man 2");
+            gameNames.Add("Spider-Man (Miles Morales)");
+
+            var names = gameNames.OrderBy(name=>name.Length);
+            foreach (var name in names)
+            {
+                Console.WriteLine(name);
+            }
+
+
+
         }
     }
 }
